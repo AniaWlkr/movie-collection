9f607d1b98d2d8a2ffbc7f0af1055c3c53ef9066
GolovinaElia <eliagolovina@ukr.net>
2021-03-27 22:02:41 +0200

    Добавляет поиск по ключевому слову в апи

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 import './sass/main.scss';
+import MoviesApiService from './js/api-service.js/apiService';
+
+const moviesApiService = new MoviesApiService();
+moviesApiService.getResponseAll().then(({ data: { results } }) => console.log(results));
+ 
+const inputRef = document.querySelector('.search-input');
+inputRef.addEventListener('input', onSearch);
+
+function onSearch(event) {
+    moviesApiService.query = event.target.value;
+    moviesApiService.getResponseWord().then(({ data: { results } }) => console.log(results));
+}
