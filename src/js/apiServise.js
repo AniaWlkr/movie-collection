df3b690e767680f3406a71610e5d0365e7b48475
GolovinaElia <eliagolovina@ukr.net>
2021-03-27 14:48:06 +0200

    Добавляет файл apiService

---
+++ src/js/apiServise.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+const BASE_URL = 'https://api.themoviedb.org/';
+const API_KEY = 'be8c1fddab60d3ca36450ce7d48f58dd';
+ 
+const url = `${BASE_URL}3/trending/all/day?api_key=${API_KEY}`;
+
+export default function getResponse(query, page) {
+    return axios.get(url)
+}
+// fetch(url).then(response => {
+//     return response.json()
+// }).then(movie => {
+//           console.log(movie);
+//     }).catch(error => {
+//             console.log(error);
+//         })
+    
