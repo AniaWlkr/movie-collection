7f04df5d788846ad7fc8e01318719da43c3ec418
Igor <Travis789@gmail.com>
2021-03-28 01:09:10 +0100

    first-project-commit

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import './sass/main.scss';
 import apiServise from './js/apiServise';
+import movieCard from './templates/movie-card.hbs';
 
 (apiServise('all', 1).then(({ data: { results } }) => console.log(results)));
