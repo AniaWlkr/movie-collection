65ecb400e9381bffe428fe39682320f3f498d344
GolovinaElia <eliagolovina@ukr.net>
2021-03-28 13:17:39 +0300

    Добавляет изменения в файл API

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import './modal_film_card/modal-film-card';
 import './modal_film_card/go-up';
 import './js/switch-page'
 import MoviesApiService from './js/api-service.js/apiService';
+import './js/switch-page';
+import movieCard from './templates/movie-card.hbs';
+import MoviesApiService from './js/api-service/apiService';
+import pagination from './js/pagination/pagination'
 
 const moviesApiService = new MoviesApiService();
 moviesApiService.getResponseAll().then(({ data: { results } }) => console.log(results));
