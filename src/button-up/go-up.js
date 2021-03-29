5643fb2803f9778ae3b6e8b0e07d05de05e78563
Elena Gubarenko <lenagritsishina@gmail.com>
2021-03-29 16:29:09 +0300

    adding changes

--- src/button-up/go-up.js
+++ src/button-up/go-up.js
@@ -8,4 +8,17 @@ const goUp = () => {
   });
 };
 
+const watchScroll = () => {
+  const scroll = window.pageYOffset;
+  const coords = document.documentElement.clientHeight;
+
+  if (scroll < coords) {
+    buttonUpRef.classList.add('visually-hidden');
+  }
+  if (scroll > coords) {
+    buttonUpRef.classList.remove('visually-hidden');
+  }
+};
+
 buttonUpRef.addEventListener('click', goUp);
+window.addEventListener('scroll', watchScroll);
