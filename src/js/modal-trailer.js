a5b83b3bffd074fb34fb408580e99bb7d850d878
Tetiana <rudenkotatyana19@gmail.com>
2021-03-31 15:05:34 +0300

    adding error handling

--- src/js/modal-trailer.js
+++ src/js/modal-trailer.js
@@ -1,8 +1,10 @@
 import MoviesApiService from './api-service/apiService';
 const moviesApiService = new MoviesApiService();
 
-const btnTrailerRef = document.querySelector('.modal-content');
-const modalTrailerRef = document.querySelector('.modal-overlay-trailer');
+const refs = {
+  btnTrailer: document.querySelector('.modal-content'),
+  modalTrailer: document.querySelector('.modal-overlay-trailer'),
+};
 
 function onOpenTrailer(event) {
   const movieId = event.target.dataset.sourse;
@@ -15,6 +17,7 @@ function onOpenTrailer(event) {
     .then(({ data: { results } }) => {
       const trailerKey = results[0].key;
 
+      refs.modalTrailer.innerHTML = '';
       const markupModalTrailer = `<div class="modal-backdrop" >
                     <div class="modal-container">
                          <iframe src="https://www.youtube.com/embed/${trailerKey}" class="trailer" frameborder="0"  allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
@@ -22,11 +25,16 @@ function onOpenTrailer(event) {
                           </iframe>
                    </div>
                   </div >`;
-      modalTrailerRef.insertAdjacentHTML('beforeend', markupModalTrailer);
-
-      document.body.classList.add('show-trailer');
+      createHTML(markupModalTrailer);
     })
     .catch(error => {
+      const markupModalImage = `<div class="modal-backdrop" >
+      <div class="modal-container">
+      <img src="https://linuxliaison.org/wp-content/uploads/2017/10/Screenshot-from-2017-10-16-23-05-56.png" class="trailer" />
+      </div>
+      </div >`;
+
+      createHTML(markupModalImage);
       console.log(error);
     });
 }
@@ -34,8 +42,13 @@ function onOpenTrailer(event) {
 function onCloseTrailer(event) {
   if (event.target) {
     document.body.classList.remove('show-trailer');
+    refs.modalTrailer.innerHTML = '';
   }
 }
+function createHTML(murkup) {
+  refs.modalTrailer.insertAdjacentHTML('beforeend', murkup);
+  refs.modalTrailer.classList.add('show-trailer');
+}
 
-btnTrailerRef.addEventListener('click', onOpenTrailer);
-modalTrailerRef.addEventListener('click', onCloseTrailer);
+refs.btnTrailer.addEventListener('click', onOpenTrailer);
+refs.modalTrailer.addEventListener('click', onCloseTrailer);
