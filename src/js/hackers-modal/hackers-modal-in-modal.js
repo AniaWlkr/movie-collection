57ff28e51477dc078768017838cde4f634b2d3e6
Igor <Travis789@gmail.com>
2021-04-05 01:17:42 +0200

    modal-in-modal-is-working

--- src/js/hackers-modal/hackers-modal-in-modal.js
+++ src/js/hackers-modal/hackers-modal-in-modal.js
@@ -4,18 +4,35 @@ import hackerCard from '../../templates/hacker-card.hbs';
 const refs = {
     list: document.querySelector('.hackers-list'),
     backdrop: document.querySelector('.hacker-modal-backdrop'),
+    modal: document.querySelector('.hacker-modal-in-modal'),
     closeModalBtn: document.querySelector('[data-action="hacker-cls-btn"]'),
+    card: '',
 }
 
 function openCardModal(event) {
     const target = event.target;
+
     if(!target.classList.contains('hackers-img')) return;
     refs.backdrop.classList.remove('hide-hacker');
-    console.log(target);
+
+    const developer = team.find(dev => dev.id === Number(target.dataset.id));
+    renderHackerCard(developer);
+    // console.log(refs.card);
+    refs.card = document.querySelector('.hacker-modal-card');
+    // console.log(refs.card);
 }
 
 function closeCardModal() {
     refs.backdrop.classList.add('hide-hacker');
+    cleanModal()
+}
+
+function renderHackerCard(developer) {
+    return refs.modal.insertAdjacentHTML('beforeend', hackerCard(developer));
+}
+
+function cleanModal() {
+    refs.modal.removeChild(refs.card)
 }
 
 refs.list.addEventListener('click', openCardModal)
